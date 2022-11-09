a2699e0a5355e5e1c2270904b8e76537a0482e8a
kavash11 <80080693+kavash11@users.noreply.github.com>
2022-11-08 20:32:20 -0500

    Insertion Sort Using Pointers

--- question4.c
+++ question4.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <stdbool.h>
 
 
-int insertionsort(int* x, int size) {
+int insertionsort(int* x, int size, int (*compare)(int a, int b)) {
     int count = 0;
+    void swap (int* num1, int* num2);
     for(int i=1;i<(size);i++) {
         int temp = x[i];
         int j = i-1;
-        while((temp<x[j]) && j>=0) {
-            x[j+1] = x[j];
+        while(((*compare)(x[j],temp) == true && j>=0)) {
+            swap(&x[j],&x[j+1]);
             j--;
             count++;
         }
-        if(temp>x[j]&& j>=0) {
+        if((*compare)(x[j],temp) == false && j>=0) {
             count++;
-        }
-        x[j+1]=temp;
+        }    
     }
     return (count);
 }
-    
-int main () {
-    int x[] = {100};
-    int size = sizeof(x)/sizeof(int);
-    printf("list %d",(insertionsort(x,size)));
-    return 0;
+
+int ascending (int a, int b) {
+    if (a<=b) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+int descending (int a, int b) {
+    if (a>=b) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+void swap (int* num1, int* num2) {
+        int temp=0;
+        temp = *num1;
+        *num1 = *num2;
+        *num2 = temp;
 }
 
+
+int main () {
+    int x[] = {548, 845, 731, 258, 809, 522, 73, 385, 906, 891, 988, 289, 808, 128};
+    printf("%d",insertionsort(x,14,ascending));
+    /*for (int i=0;i<14;i++) {
+        printf("%d\n",x[i]);
+    } */   
+    return 0;
+}
